0f0dc0ba9d917ef672f8bafeee3fce44c795b672
rikuhen <jorgeconsalvacion@gmail.com>
2017-03-01 16:23:40 -0500

    refactor(auth:admin): change path and names

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,16 @@ Route::get('/', function () {
 
 
 Route::group(['prefix'=>'backend'],function(){
-	Auth::routes();
 	Route::get('/',function(){
 		if (Auth::guest()) return redirect('/backend/login');
 		return redirect('/backend/users'); 
 	});
 
+	// Authentication Routes...
+    Route::get('login', 'Auth\LoginAdminController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginAdminController@login');
+    Route::post('logout', 'Auth\LoginAdminController@logout')->name('logout');
+    //resources
 	Route::resource('/users', 'UserController');
 	Route::resource('/clients', 'ClientController');
 	Route::resource('/misses', 'MissController');
