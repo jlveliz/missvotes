21b8233666d64c580d6c72566488add66433a241
Jorge Luis Veliz B <Jorge Luis Veliz B>
2017-03-29 22:16:15 -0500

    changes

--- resources/views/Backend/miss/index.blade.php
+++ resources/views/Backend/miss/index.blade.php
@@ -26,7 +26,7 @@
      			@foreach ($misses as $miss)
        			<tr>
        				<td>{{$miss->name}} {{$miss->last_name}}</td>
-       				<td>{{$miss->country->name}}</td>
+       				<td>@if($miss->country) {{$miss->country->name}} @else - @endif</td>
        				<td>@if($miss->state == '1') Activa @else Inactiva @endif</td>
        				<td>{{$miss->created_at }} / {{$miss->updated_at}}</td>
        				<td class="text-center">
