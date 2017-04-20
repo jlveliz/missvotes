7a4ac35032ef5f136898d6fb1ee86b5ae706b4b4
Luis Lazarous <Luis Lazarous>
2017-04-20 14:01:52 -0500

    fet(lang): locate

---
+++ resources/lang/en/membership.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+	
+	'free_lbl' => 'Free',
+	'act_membership_lbl' => 'Your current memebership',
+	'lbl_membership_lbl' => 'For',
+	'tm_membership_lbl' => 'year',
+	'includes_membership_lbl' => 'Includes',
+	'free_1_membership_lbl' => 'Special news about our candidates.',
+	'free_2_membership_lbl' => 'You can vote online for your favorite candidate. Your daily vote equal to 1 point.',
+	'free_3_membership_lbl' => 'Access to our promotions for a whole year.',
+	'update_btn' => 'Upgrade',
+	'txt_data_1' => 'This year the organization will allow one of the candidates to go directly to the top 12 in the final event. Everyone can vote but must be subscribed to our website to access the online voting. There are two types of subscriptions:',
+	'prm_1_membership_lbl' => 'Special news about our candidates.',
+	'prm_2_membership_lbl' => 'You can vote online for your favorite candidate. Your daily vote will equal 3 points.',
+	'prm_3_membership_lbl' => 'Access to our promotions for a whole year.',
+	'prm_4_membership_lbl' => 'A percentage of the premium subscription value will be donated to Junior Foundation: children with cancer.',
+	'free_sus' => 'Free Subscription',
+	'txt_free_sus' => 'Each user can vote once a day, every 24 hours. In this type of subscription your daily vote equals 1 point.',
+	'prem_sus' => 'Premium Subscription',
+	'txt_prem_sus' => 'If you want your favorite candidate to be closer to the international crown, you have the option to subscribe in the premium category, which will allow you to vote once a day but your daily vote will equal 3 points.',
+	'adi_txt' => 'The candidate who accumulates more points, will enter directly to the top 12.',
+
+];
--- resources/views/frontend/pages/apply/requirements.blade.php
+++ resources/views/frontend/pages/apply/requirements.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.frontend')
 @section('content')
 	<div class="row">
-		<h1 class="text-center">Requisitos de Aplicación</h1>
+		<h1 class="text-center">@lang('requirement.tittle_process')</h1>
 		<div class="col-md-6 col-lg-6 col-xs-12 col-md-offset-3">
 			@if (Session::has('message'))
         		<div class="alert alert-dismissible @if(Session::get('type') == 'success') alert-info  @endif @if(Session::get('type') == 'error') alert-danger  @endif" role="alert">
@@ -11,32 +11,32 @@
         		<div class="clearfix"></div>
         	@endif
 			<ol>
-				<li>Ser originaria del país que representa.
-				<li>Ser mujer de nacimiento.</li>
-				<li>Conocer la historia, situación económica y política de su país.</li>
-				<li>Tener edad entre 18 a 28 años: La candidata debe tener 18 años o tenerlo antes del evento. La candidata debe tener 28 años de edad al iniciar el evento y cumplir 29 años antes de octubre del año siguiente.</li>
-				<li>Tener estudio mínimo: Preparatoria (High School) o equivalente</li>
-				<li>Tener estatura mínima sin zapatos:  5.5 ft / 1.67 cm (no menos que la estatura aquí establecida)</li>
-				<li>Tener hermosa armonía estética de rostro y cuerpo. </li>
-				<li>Gozar de perfecta salud física y mental. </li>
-				<li>Tener el peso proporcionado de acuerdo a su estatura. </li>
-				<li>Nunca haber sido casada. </li>
-				<li>Nunca haber tenido hijos. </li>
-				<li>Tener facilidad de expresión corporal y oral. </li>
-				<li>Contar con pasaporte vigente del país de origen o certificado de nacimiento</li>
-				<li>Tener buenos modales y que no haber cometido felonías. </li>
-				<li>Desarrollar una idea turística sobre su país de origen.</li>
-				<li>Estar de acuerdo con las reglas y disciplina del certamen Miss Panamerican International.</li>
+				<li>@lang('requirement.txt_01')</li>
+				<li>@lang('requirement.txt_02')</li>
+				<li>@lang('requirement.txt_03')</li>
+				<li>@lang('requirement.txt_04')</li>
+				<li>@lang('requirement.txt_05')</li>
+				<li>@lang('requirement.txt_06')</li>
+				<li>@lang('requirement.txt_07')</li>
+				<li>@lang('requirement.txt_08')</li>
+				<li>@lang('requirement.txt_09')</li>
+				<li>@lang('requirement.txt_10')</li>
+				<li>@lang('requirement.txt_11')</li>
+				<li>@lang('requirement.txt_12')</li>
+				<li>@lang('requirement.txt_13')</li>
+				<li>@lang('requirement.txt_14')</li>
+				<li>@lang('requirement.txt_15')</li>
+				<li>@lang('requirement.txt_16')</li>
 			</ol>
 			<form action="{{ route('apply.aceptrequirements') }}" method="POST">
 				{{ csrf_field() }}
 				<div class="checkbox">
 				    <label>
-				      <input type="checkbox" name="acept-terms" value="1"> La candidata acepta poseer las características y requisitos mencionados arriba para representar a su país. 
+				      <input type="checkbox" name="acept-terms" value="1"> @lang('requirement.txt_terms')
 				    </label>
 				</div>
 				<div class="text-center">
-					<button type="submit" class="btn btn-primary">Aceptar </button>
+					<button type="submit" class="btn btn-primary">@lang('requirement.btn_accept') </button>
 				</div>
 			</form>
 			<br><br>
