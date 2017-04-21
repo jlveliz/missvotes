5e1580d8e36a4057e5e1b46735ed74724cfba33b
rikuhen <jorgeconsalvacion@gmail.com>
2017-04-21 08:30:03 -0500

    changes email

--- resources/lang/en/email.php
+++ resources/lang/en/email.php
@@ -9,7 +9,23 @@ return [
 		'casting_code' => 'YOUR CASTING CODE IS: ',
 		'title_data' => 'Below we detail the place, the days, the schedules and the costume that you must take to the casting:',
 		'place' => 'PLACE',
-		'days_hours_casting' =>'DAYS AND CASTING HOURS', 
+		'days_hours_casting' =>'DAYS AND CASTING HOURS',
+		'weekend_day_a'=>'Saturday, May 6 from 2pm to 6pm' ,
+		'weekend_day_b'=>'Sunday, May 7 from 2pm to 6pm' ,
+		'weekend_day_c'=> 'Saturday, May 13 from 2pm to 6pm',
+		'weekend_day_d'=> 'Sunday, May 14 from 2pm to 6pm',
+		'weekend_day_e'=> 'Saturday, May 20 from 2pm to 6pm',
+		'weekend_day_f'=> 'Sunday, May 21 from 2pm to 6pm',
+		'weekend_day_g'=> 'Saturday, May 27 from 2pm to 6pm',
+		'weekend_day_h'=> 'Sunday, May 28 from 2pm to 6pm',
+		'costume' => 'COSTUME',
+		'costume_1'=>'1 Two piece swimsuit',
+		'costume_2'=>'1 pair of high-heels shoes (that you know to wear)',
+		'costume_3'=>'1 cocktail dress of your choice',
+		'costume_obs_1'=>'YOU CAN ATTEND ANY OF THE DAYS MENTIONED HERE, BUT WITHIN THE SCHEDULE ESTABLISHED.',
+		'costume_obs_2'=>'FOR CASTING YOU MUST MENTION YOUR CASTING NUMBER. WE WAIT FOR YOU AND GOOD LUCK.',
+		'questions'=>'Any questions, call us to our office number: +1 (951)734-2360',
+
 	],
 
 	'verify' => [
@@ -17,6 +33,8 @@ return [
 		'thanks' => ' Thank you for registering. Now you just activate your account so you can know more about us and of course to vote for your favorite candidate.',
 		'activate_button' => 'Activate Account',
 		'if_prefeer' => 'If you prefer, please copy the following link and paste it into your browser',
+
+
 	],
 
 ];
--- resources/views/frontend/emails/casting.blade.php
+++ resources/views/frontend/emails/casting.blade.php
@@ -11,22 +11,40 @@
         <div>
             <p>{{ trans('email.casting.hi') }} <b>{{$precandidate->name}} {{$precandidate->last_name}}</b>, {{ trans('email.casting.thanks') }} <b> {{ trans('email.casting.welcome') }}</p>
             <br>
-            <br>
             <h2>{{ trans('email.casting.casting_code') }} {{ $precandidate->code }}</h2>
             <br>
-            <br>
-
+            
             <p>{{ trans('email.casting.title_data') }}</p>
 
-            <br>
             <br>
 
             <p><u>{{ trans('email.casting.place') }}</u></p>
             <p>Moda 2000 <br> 845 N. Euclid St. Anaheim CA 92801</p>
+            <p>845 N. Euclid St. Anaheim CA 92801</p>
 
             <br>
-            <br>
             <p><u>{{ trans('email.casting.days_hours_casting') }}</u></p>
+            <p>{{ trans('email.casting.weekend_day_a') }}</p>
+            <p>{{ trans('email.casting.weekend_day_b') }}</p>
+            <br>
+            <p>{{ trans('email.casting.weekend_day_c') }}</p>
+            <p>{{ trans('email.casting.weekend_day_d') }}</p>
+            <br>
+            <p>{{ trans('email.casting.weekend_day_e') }}</p>
+            <p>{{ trans('email.casting.weekend_day_f') }}</p>
+            <br>
+            <p>{{ trans('email.casting.weekend_day_g') }}</p>
+            <p>{{ trans('email.casting.weekend_day_h') }}</p>
+            <br>
+            <p><u>{{ trans('email.casting.costume') }}</u></p>
+            <p>{{ trans('email.casting.costume_1') }}</p>
+            <p>{{ trans('email.casting.costume_2') }}</p>
+            <p>{{ trans('email.casting.costume_3') }}</p>
+            <br>
+            <p>{{ trans('email.casting.costume_obs_1') }}</p>
+            <p>{{ trans('email.casting.costume_obs_2') }}</p>
+            <br>
+            <p>{{ trans('email.casting.questions') }}</p>
             
         </div>
 
