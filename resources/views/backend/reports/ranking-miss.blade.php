1085bd74c53bd9cc44ede3fd3412eaeb1000e5b2
rikuhen <jorgeconsalvacion@gmail.com>
2017-05-18 09:14:59 -0500

    style(miss): cambios en vote

--- resources/views/backend/reports/ranking-miss.blade.php
+++ resources/views/backend/reports/ranking-miss.blade.php
@@ -14,7 +14,7 @@
 	  	 		<tr @if ($index == 0) class="success" @endif>
 	  				<td>@if ($index == 0) <b>{{$vote->miss->name}} {{$vote->miss->last_name}}</b> @else {{$vote->miss->name}} {{$vote->miss->last_name}} @endif </td>
 	  				<td>@if ($index == 0) <b>{{$vote->miss->country->name}}</b> @else {{$vote->miss->country->name}} @endif</td>
-	  				<td>@if ($index == 0) <b>{{$vote->sumatory}} {{ trans('backend.dashboard.ranking_block.points') }}</b> @else {{$vote->sumatory}} {{ trans('backend.dashboard.ranking_block.points') }} @endif </td>
+	  				<td>@if ($index == 0) <b>{{$vote->sumatory}} {{ trans('backend.dashboard.ranking_block.td_points') }}</b> @else {{$vote->sumatory}} {{ trans('backend.dashboard.ranking_block.td_points') }} @endif </td>
 	  			</tr>
 	  		@endforeach
   		</tbody>
