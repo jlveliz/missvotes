92f327702272e9e57a272d9a89cdca25d0126b91
Jorge Veliz <jorgeconsalvacion@gmail.com>
2017-12-23 23:07:40 -0500

    lang

--- resources/lang/es/auth.php
+++ resources/lang/es/auth.php
@@ -1,185 +1,370 @@
 <?php
 
+
+
 return [
 
+
+
     /*
+
     |--------------------------------------------------------------------------
+
     | Authentication Language Lines
+
     |--------------------------------------------------------------------------
+
     |
+
     | The following language lines are used during authentication for various
+
     | messages that we need to display to the user. You are free to modify
+
     | these language lines according to your application's requirements.
+
     |
+
     */
+
     'login_title' => 'Ingresar',
+
     'login_fields' => [
+
         'email' => 'Correo Electrónico',
+
         'password' => 'Contraseña',
+
         'remember_me' => 'Recordarme'
-    ],
-    'login_options' => [
-        'login_button' => 'Ingresar',
-        'go_register' => 'Suscribirse',
-        'forgot_password' => 'Olvidaste tu contraseña?',
-        'code_not_recivied' => "No recibió el código de Activación?",
-        'message_close_session'=> 'Si quieres suscribirte con otro correo electrónico, primero  debes cerrar tu sesión ',
-        'message_app_session'=> 'Para tener acceso a la aplicación en línea debes suscribirte primero / Si ya eres un usuario ingresa a tu cuenta ',
+
     ],
 
+    'login_options' => [
+
+        'login_button' => 'Ingresar',
+
+        'go_register' => 'Suscribirse',
+
+        'forgot_password' => 'Olvidaste tu contraseña?',
+
+        'code_not_recivied' => "No recibió el código de Activación?",
+
+        'message_close_session'=> 'Si deseas postularte en nuestro casting internacional, debes suscribirte primero para tener acceso a la aplicaci&oacute;n en l&iacute;nea.',
+
+        'message_app_session'=> 'Para tener acceso a la aplicación en línea debes suscribirte primero / Si ya eres un usuario ingresa a tu cuenta ',
+
+    ],
+
+
+
     'failed' => 'contraseña o correo incorrecto. Vuelva a intentarlo.',
+
     'throttle' => 'Ha sobrepasado el máximo número de intengos de ingreso, intente en :seconds segundos.',
+
     'not_confirmed' => 'Lo sentimos, tu cuenta está inactiva.',
 
 
 
 
+
+
+
+
+
     /***
+
     /
+
     / REGISTER
+
     /
+
     ***/
 
+
+
     'register_title' => 'Suscribirse',
+
     'register_fields' => [
+
         'email' => 'Correo Electrónico',
+
         'name' => 'Nombre',
+
         'last_name' => 'Apellido',
+
         'country-select' => 'Seleccione un País',
+
         'city' => 'Ciudad',
+
         'address' => 'Dirección',
+
         'password' => 'Contraseña',
+
         'confirm_password' => 'Confirmar Contraseña',
+
         'gender' => 'Género',
+
         'male' => 'Masculino',
+
         'female' => 'Femenino',
+
         'accept_terms' => 'Acepto los',
-        'terms_conditions' => 'terminos y condiciones',
+
+        'terms_conditions' => 't&eacute;rminos y condiciones',
+
         'follows' => 'Síguenos'
+
     ],
+
     'register_options' => [
+
         'register' => 'Suscribirse',
+
         'login' => 'Ingresar'
+
     ],
+
+
 
     'validations_register' => [
+
         'name.required' => 'Por favor, Ingrese su nombre',
+
         'name.max' => 'Por favor, ingrese un nombre mas corto',
+
         'last_name.required' => 'Por favor, Ingrese su apellido',
+
         "country_id.required" => 'Por favor, ingrese un País',
+
         "country_id.exists" => 'El País no existe',
+
         "city.required" => 'La Ciudad es requerida',
+
         'email.required' => 'Por favor ingrese un correo',
+
         'email.email' => 'Por favor ingrese un correo válido',
+
         'email.max' => 'Por favor su correo es muy grande',
+
         'email.unique' => 'El correo ya se encuentra registrado',
+
         'address.required' => 'Por favor ingrese una dirección',
+
         'password.required' =>'Por favor ingrese una contraseña',
+
         'password.min' => 'Por favor ingrese una contraseña más larga',
+
         'password.confirmed' => 'Las contraseñas no coinciden',
+
         'password_confirmation.required' => 'Por favor repita la contraseña',
+
         'accept_terms_conditions.required' => 'Por favor, acepte los terminos y condiciones'
+
     ],
 
+
+
     'register_success' => 'Registro completo!',
+
     'register_success_message'=>'Su cuenta se creó correctamente, pero debemos verificar su correo electrónico. Hemos enviado un correo de confirmación a su bandeja de entrada. Simplemente debe hacer clic en el enlace que reciba para activar su cuenta. No olvide buscar también en su bandeja de correos no deseados. ¡Gracias!',
+
     'register_success_submit' => 'Aceptar',
 
 
 
 
+
+
+
+
+
      /***
+
     /
+
     / FORGOT PASSWORD
+
     /
+
     ***/
+
+
+
 
 
     'forgot_password_title' => 'Olvidó su Contraseña',
+
     'forgot_password_fields' => [
+
         'email' => 'Email'
+
     ],
+
     'forgot_password_options' => [
+
         'reset_password' => 'Resetear Contraseña'
+
     ],
+
     'forgot_password_message' => 'Se ha enviado el correo para poder cambiar su contraseña, no olvide de revisar en su bandeja de correo no deseado. Por favor reviselo y siga las instrucciones.',
+
     'forgot_password_validations' => [
+
         'email.required' => 'Por favor, ingrese un correo',
+
         'email.exists' => 'El correo no pertenece a ningún usuario',
+
         'email.confirmed_account' => 'Lo sentimos, su cuenta está inactiva.'
+
     ],
+
+
 
     
+
     /***
+
     /
+
     / ACTIVATION
+
     /
+
     ***/
+
     'activation_page_title' => 'Activación de Usuario',
+
     'activation_page_description' => 'El código de activación puede demorar hasta 30 minutos en llegar. Revise en su bandeja de correo no deseados.',
+
     'activation_page_message' => 'Se ha enviado el correo de activación. Por favor revise su correo y no olvide de revisar en su bandeja de correo no deseado. ',
+
     'activation_page_fields' =>[
+
         'email' => 'Correo Electrónico'
+
     ],
+
     'activation_page_options' =>[
+
         'submit' => 'Enviar email de confirmación'
+
     ],
+
     'validations_activation' => [
+
         'email.required' => 'Por favor ingrese un Correo Electrónico',
+
         'email.exists' => 'El correo Electrónico ya pertenece a Usuario',
+
         'email.is_confirmed_account' => 'Su cuenta ya ha sido confirmada',
+
     ],
+
     'activation_success_message' => 'La cuenta ha sido activada correctamente.',
+
     'activation_error_message' => 'La cuenta no pudo ser activada.',
+
     'activation_loading' => 'Cargando...',
 
+
+
   /***
+
     /
+
     / RESET 
+
     /
+
     ***/
+
     'reset_password_title' => 'Cambiar Contraseña',
+
     'reset_password_fields' => [
+
         'email' =>'Correo Electrónico',
+
         'password' => 'Contraseña',
+
         'confirm_password' =>'Confirmar Contraseña'
+
     ],
+
     'reset_password_options' => [
+
         'submit' => 'Cambiar Contraseña'
+
     ],
+
     'reset_password_validations' => [
+
         'email.token' => 'El token es inválido',
+
         'email.required' => 'Por favor, ingrese un Correo Electrónico',
+
         'email.email' => 'Por favor, ingrese un correo válido',
+
         'password.required' => 'Por favor, ingrese una nueva contraseña',
+
         'password.confirmed' => 'Las contraseñas no coinciden',
+
         'password.min' => 'Por favor ingrese una contraseña de al menos 6 caracteres',
+
     ],
+
+
+
+
 
 
 
     'profile' => [
+
         'validations'=>[
+
             'name.required'=>'Su nombre es requerido',
+
             'last_name.required'=>'Su apellido es requerido',
+
             'country_id.required' => 'El País es requerido',
+
             'country_id.exists' =>'El País no existe',
+
             'city.required'=>'La ciudad es requerida',
+
             'address.required' => 'La dirección es requerida',
+
             'password.required_with' => 'Por favor ingrese una contraseña',
+
             'password.min' => 'Ingrese una contraseña mas larga por favor',
+
             'repeat_password.same' => 'La contraseñas no coinciden',
+
             'repeat_password.required_with' => 'Por favor ingrese la confirmación de contraseña',
+
             'photo.required_with'=> 'Inserte una imagen',
+
             'photo.image' => 'Inserte una imagen'   
-        ],
-        'change_password' => [
-            'cant_change' => 'Su contraseña no pudo ser actualizada',
-            'change_success' => 'Su Contraseña fue actualizada',
 
         ],
+
+        'change_password' => [
+
+            'cant_change' => 'Su contraseña no pudo ser actualizada',
+
+            'change_success' => 'Su Contraseña fue actualizada',
+
+
+
+        ],
+
         'update_profile' => 'Perfil Actualizado correctamente',
+
     ],
 
 
+
+
+
 ];
+
--- resources/lang/es/membership.php
+++ resources/lang/es/membership.php
@@ -1,27 +1,53 @@
 <?php
 
+
+
 return [
+
 	
+
 	'free_lbl' => 'Gratuito',
+
 	'act_membership_lbl' => 'Su membresía actual',
+
 	'lbl_membership_lbl' => 'Duración',
+
 	'tm_membership_lbl' => 'año',
+
 	'includes_membership_lbl' => 'Incluye',
-	'free_1_membership_lbl' => 'Noticias sobre nuestras candidatas.',
-	'free_2_membership_lbl' => 'Podrás votar online por tu candidata favorita. Tu votación diaria equivaldrá a 1 punto.',
-	'free_3_membership_lbl' => 'Acceso a nuestras promociones o sorteos durante todo un año.',
+
+	'free_1_membership_lbl' => '<b>Noticias:</b> Todo lo relacionado a las actividades de nuestras candidatas.',
+
+	'free_2_membership_lbl' => '<b>Votaci&oacute;n Online de 1 punto:</b> Cuando se abran las votaciones podr&aacute;s votar hasta 5 veces al d&iacute;a por tu candidata favorita. Tu suscripci&oacute;n Gratuita te permite que cada voto valga 1 punto.',
+
+	'free_3_membership_lbl' => '<b>Sorteos y Promociones:</b> Acceso a nuestras promociones o sorteos durante todo un a&ntilde;o.',
+
 	'update_btn' => 'Actualizar',
+
 	'txt_data_1' => 'Este año la organización permitirá que una de las candidatas entre directamente al top 12 en el evento final. Todos podrán votar pero deben estar suscritos a nuestra pagina web para acceder a las votaciones online. Hay dos tipos de suscripciones:',
-	'prm_1_membership_lbl' => 'Noticias sobre nuestras candidatas.',
-	'prm_2_membership_lbl' => 'Podrás votar online por tu candidata favorita. Tu votación diaria equivaldrá a 3 puntos.',
-	'prm_3_membership_lbl' => 'Acceso a nuestras promociones o sorteos durante todo un año.',
-	'prm_4_membership_lbl' => 'Un porcentaje del valor de la suscripción premium se donará a Junior Foundation: niños con cáncer.',
+
+	'prm_1_membership_lbl' => '<b>Noticias Exclusivas:</b> Ser&aacute;s parte del grupo de personas que tendr&aacute;n informaci&oacute;n detallada y m&aacute;s reciente sobre nuestras candidatas oficiales.',
+
+	'prm_2_membership_lbl' => '<b>Votaci&oacute;n Online de 3 puntos:</b> Cuando se abran las votaciones podr&aacute;s votar hasta 5 veces al d&iacute;a por tu candidata favorita. Tu suscripci&oacute;n Premium te permite que cada voto valga 3 puntos.',
+
+	'prm_3_membership_lbl' => '<b>Sorteos y Promociones:</b> Tendr&aacute;s acceso exclusivo a nuestras promociones y sorteos realizados solo para nuestros suscriptores Premium durante todo un a&ntilde;o. Tambi&eacute;n, podr&aacute;s participar en los sorteos y promociones realizadas para los suscriptores gratuitos.',
+
+	'prm_4_membership_lbl' => '<b>Donaci&oacute;n:</b> Un porcentaje del valor de la suscripci&oacute;n Premium se donar&aacute; a Junior Foundation: ni&ntilde;os con c&aacute;ncer.',
+
 	'free_sus' => 'Suscripción Gratuita',
+
 	'txt_free_sus' => 'Cada usuario podrá votar una sola vez al día, es decir cada 24 horas.  En este tipo de suscripción tu votación diaria equivale a 1 punto.',
+
 	'prem_sus' => 'Suscripción Premium',
+
 	'txt_prem_sus' => 'Si quieres que tu candidata favorita esté mas cerca de la corona internacional, tienes la opción de suscribirte en categoría premium, lo cual te permitirá votar una sola vez al día, es decir cada 24 horas,  pero tu votación diaria equivaldrá a 3 puntos.',
+
 	'adi_txt' => 'La candidata que acumule mas puntos, entrará directamente al top 12.',
 
+
+
 	'thanks_premium_membership' => 'Gracias por ser parte de nuestra familia Premium',
+
+
 
 ];
