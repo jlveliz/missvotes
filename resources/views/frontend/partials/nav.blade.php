6ec0324eb0822372555ca7297fca24cb28d2b7ac
Jorge Veliz <jorgeconsalvacion@gmail.com>
2017-12-25 21:20:31 -0500

    feat(commit)

--- resources/views/frontend/partials/nav.blade.php
+++ resources/views/frontend/partials/nav.blade.php
@@ -59,25 +59,23 @@
                     </form>
                 </li>
 
-                {{-- @if (App::isLocale('en')) --}}
-                    {{-- <li> --}}
-                        {{-- <form action="{{ route('website.locale') }}"> --}}
-                            {{-- {{ csrf_field() }} --}}
-                            {{-- <input type="hidden" name="lang" value="es"> --}}
-                            {{-- <button  type="submit" class="btn btn-update-membership-or-buy" > Spanish</button> --}}
-                            
-                        {{-- </form> --}}
-                    {{-- </li> --}}
-                {{-- @else --}}
-{{--                     <li>
+                @if (App::isLocale('en'))
+                    <li>
                         <form action="{{ route('website.locale') }}">
- --}}                            {{-- {{ csrf_field() }} --}}
-{{--                             <input type="hidden" name="lang" value="en">
-                            <button  type="submit" class="btn btn-update-membership-or-buy"> Inglés</button>
-                            
+                            {{ csrf_field() }}
+                            <input type="hidden" name="lang" value="es">
+                            <button  type="submit" class="btn btn-update-membership-or-buy" > Spanish</button>                            
                         </form>
-                    </li> --}}
-                {{-- @endif --}}
+                    </li>
+                @else
+                    <li>
+                        <form action="{{ route('website.locale') }}">
+                            {{ csrf_field() }}
+                            <input type="hidden" name="lang" value="en">
+                            <button  type="submit" class="btn btn-update-membership-or-buy"> Inglés</button>                 
+                        </form>
+                    </li>
+                @endif
 
                {{--  <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
